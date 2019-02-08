b11d94860678de0166959a6b34c88ffc0368d707
Jinzhe <jinzhe@simple.com.au>
2019-02-08 13:29:29 +1030

    Add more comment to example config

--- src/config.example.php
+++ src/config.example.php
@@ -4,9 +4,13 @@
  */
 return [
     [
+        /* The name of the endpoint, used as a label */
         'name' => 'Example Endpoint Name',
+        /* The actual url you'd like to guard */
         'url' => 'https://jsonplaceholder.typicode.com/posts',
+        /* The amount of seconds the cache is valid for */
         'cacheSeconds' => 43200,
+        /* The slug is the unique identifier for each url, it will be used to generate the new url */
         'slug' => 'endpoint-slug'
     ],
 ];
